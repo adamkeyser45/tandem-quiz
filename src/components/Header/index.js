ffa0a94c948599480683f0aa95e30795ba8866c0
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-10-30 13:47:25 -0500

    fixed buttons in card header, ui should be complete

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,43 +4,8 @@ function Header() {
     return (
         <header style={{backgroundColor: '#fff4e6'}}>
             <nav className='navbar justify-content-center'>
-                    <h1 className='h2 m-2 '><i class="fas fa-user-graduate"></i> The Trivia Quiz!</h1>
-                    <ul className='nav justify-content-end'>
-                        <li className='nav-item'>
-                            <div class="dropdown">
-                                <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Options
-                                </button>
-                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <button type='button' className='btn btn-link' data-toggle='modal' data-target='#exampleModal'>High Scores</button>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
+                    <h1 className='h2 m-2 '><i className="fas fa-user-graduate"></i> The Trivia Quiz!</h1>
             </nav>
-        
-            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">High Scores!</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        Placeholder high Scores
-                        <ol>
-                            <li>Bill: 49</li>
-                            <li>Ted: 45</li>
-                            <li>Jill: 43</li>
-                            <li>Winifred: 40</li>
-                        </ol>
-                    </div>
-
-                    </div>
-                </div>
-            </div>
         </header>
     )
 }
--- src/components/QuizBody/index.js
+++ src/components/QuizBody/index.js
@@ -3,22 +3,75 @@ import React from 'react';
 function QuizBody() {
     return (
         <div className="contrainer-fluid" >
-            <div className='card border m-4' style={{height: '400px'}}>
+            <div className='card border m-4' style={{height: '425px'}}>
                 <div className='card-header text-center'>
-                    <span className='btn btn-light'>
+                    <div className='btn-group'>
+                    <button className='btn btn-light border'>
                         Press to Start!
-                    </span>
-                    
+                    </button>
+                    <div className="dropdown">
+                        <button className="btn btn-light border dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Options
+                        </button>
+                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <button type='button' className='btn btn-link' data-toggle='modal' data-target='#highScoreModal'>High Scores</button>
+                            <button type='button' className='btn btn-link' data-toggle='modal' data-target='#rulesModal'>How To Play</button>
+                        </div>
+                    </div>
+                    </div>
                 </div>
 
+                <div className="modal fade" id="highScoreModal" tabIndex="-1" role="dialog" aria-labelledby="highScoreModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="highScoreModalLabel">High Scores!</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            Placeholder high Scores
+                            <ol>
+                                <li>Bill: 49</li>
+                                <li>Ted: 45</li>
+                                <li>Jill: 43</li>
+                                <li>Winifred: 40</li>
+                            </ol>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="modal fade" id="rulesModal" tabIndex="-1" role="dialog" aria-labelledby="rulesModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="rulesModalLabel">How to Play!</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            Rules
+                            <ol>
+                                <li>Press Start to start the quiz</li>
+                                <li>Press the button that has the correct answer for each question</li>
+                                <li>When all of the questions are done, see your score!</li>
+                            </ol>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                
                 <div className='card-body'>
-                    <h5 class="card-title text-center h3">Question 1</h5>
-                    <p class="card-text text-center h5 border rounded p-2">What was Tandem's previous name?</p>
+                    <h5 className="card-title text-center h3">Question 1</h5>
+                    <p className="card-text text-center h5 border rounded p-2">What was Tandem's previous name?</p>
                     <div className='container pt-2'>
-                        <button class="btn btn-primary btn-block m-1">Tandem</button>
-                        <button class="btn btn-primary btn-block m-1">Burger Shack</button>
-                        <button class="btn btn-primary btn-block m-1">Extraordinary Humans</button>
-                        <button class="btn btn-primary btn-block m-1">Devmynd</button>
+                        <button className="btn btn-primary btn-block m-1">Tandem</button>
+                        <button className="btn btn-primary btn-block m-1">Burger Shack</button>
+                        <button className="btn btn-primary btn-block m-1">Extraordinary Humans</button>
+                        <button className="btn btn-primary btn-block m-1">Devmynd</button>
                     </div>
                 </div>
             </div>
