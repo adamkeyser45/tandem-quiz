7372cfac630252c4837ae48ef17cf6de4e722ac9
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-10-30 11:53:22 -0500

    mobile ui complete

--- src/components/QuizBody/index.js
+++ src/components/QuizBody/index.js
@@ -5,13 +5,21 @@ function QuizBody() {
         <div className="contrainer-fluid" >
             <div className='card border m-4' style={{height: '400px'}}>
                 <div className='card-header text-center'>
-                    Let's Play!
+                    <span className='btn btn-light'>
+                        Press to Start!
+                    </span>
+                    
                 </div>
 
                 <div className='card-body'>
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title text-center h3">Question 1</h5>
+                    <p class="card-text text-center h5 border rounded p-2">What was Tandem's previous name?</p>
+                    <div className='container pt-2'>
+                        <button class="btn btn-primary btn-block m-1">Tandem</button>
+                        <button class="btn btn-primary btn-block m-1">Burger Shack</button>
+                        <button class="btn btn-primary btn-block m-1">Extraordinary Humans</button>
+                        <button class="btn btn-primary btn-block m-1">Devmynd</button>
+                    </div>
                 </div>
             </div>
         </div>
