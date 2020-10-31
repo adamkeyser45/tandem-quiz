206a7b1913ed133e77b6e3adb7246d6e36a9ec73
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-10-31 13:44:02 -0500

    using state to update button text

--- src/components/QuizBody/index.js
+++ src/components/QuizBody/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function QuizBody() {
+
+    const [button1Text, setButton1Text] = useState('Answer Choice');
+    const [button2Text, setButton2Text] = useState('Answer Choice');
+    const [button3Text, setButton3Text] = useState('Answer Choice');
+    const [button4Text, setButton4Text] = useState('Answer Choice');
+
+    const questionsArray = require('../../assets/Apprentice_TandemFor400_Data.json');
+    
+    const button1 = (text) => setButton1Text(text);
+    const button2 = (text) => setButton2Text(text);
+    const button3 = (text) => setButton3Text(text);
+    const button4 = (text) => setButton4Text(text);
+    
+    function testClick() {
+        console.log(questionsArray);
+        let num = Math.floor(Math.random() * (questionsArray.length + 1));
+        
+        button1(questionsArray[num].incorrect[0]);
+        button2(questionsArray[num].incorrect[1]);
+        button3(questionsArray[num].incorrect[2]);
+        button4(questionsArray[num].incorrect[3]);
+    };
+
     return (
         <div className="contrainer-fluid" >
             <div className='card border m-4' style={{height: '425px'}}>
                 <div className='card-header text-center'>
                     <div className='btn-group'>
-                    <button className='btn btn-light border' id='startBtn'>
+                    <button className='btn btn-light border' id='startBtn' onClick={testClick}>
                         Press to Start!
                     </button>
                     <div className="dropdown">
@@ -68,10 +91,10 @@ function QuizBody() {
                     <h5 className="card-title text-center h3">Question <span id='questionNum'>#</span></h5>
                     <p id='question' className="card-text text-center h5 border rounded p-2">And your first question is...</p>
                     <div className='container pt-2'>
-                        <button id='answerChoice1' className="btn btn-primary btn-block m-1">Answer 1</button>
-                        <button id='answerChoice2' className="btn btn-primary btn-block m-1">Answer 2</button>
-                        <button id='answerChoice3' className="btn btn-primary btn-block m-1">Answer 3</button>
-                        <button id='answerChoice4' className="btn btn-primary btn-block m-1">Answer 4</button>
+                        <button id='answerChoice1' className="btn btn-primary btn-block m-1">{button1Text}</button>
+                        <button id='answerChoice2' className="btn btn-primary btn-block m-1">{button2Text}</button>
+                        <button id='answerChoice3' className="btn btn-primary btn-block m-1">{button3Text}</button>
+                        <button id='answerChoice4' className="btn btn-primary btn-block m-1">{button4Text}</button>
                     </div>
                 </div>
             </div>
